e7f3f095bc8a2a82cd29f9ee9987ea9ceb169ed4
Arnau <a>
2021-04-28 13:34:40 +0200

    Ejercicio 6 (sin parte grafica)

--- EjemplosClase/AA3_Client/Client.cpp
+++ EjemplosClase/AA3_Client/Client.cpp
@@ -1,24 +1,156 @@
 #include <string>
+#include <thread>
+#include <iostream>   
+#include <vector>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 
 
 sf::UdpSocket udpSocket;
+sf::UdpSocket::Status socketStatus = sf::UdpSocket::Status::NotReady;
 sf::IpAddress serverIP = "localhost";
 unsigned short serverPort = 50000;
+
+
+
+void Send(sf::IpAddress _serverIP, unsigned short _serverPort)
+{
+	while (true)
+	{
+		sf::Packet pack;
+		std::string message;
+
+		std::getline(std::cin, message);
+		pack << message;
+
+		socketStatus = udpSocket.send(pack, _serverIP, _serverPort);
+	}
+}
+
+void LoadChatWindow()
+{
+
+}
+
+//void UpdateChatWindow()
+//{
+//	std::vector<std::string> aMensajes;
+//
+//	sf::Vector2i screenDimensions(800, 600);
+//
+//	sf::RenderWindow window;
+//	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+//
+//	sf::Font font;
+//	if (!font.loadFromFile("courbd.ttf"))
+//	{
+//		std::cout << "Can't load the font file" << std::endl;
+//	}
+//
+//	sf::String mensaje = " >";
+//
+//	sf::Text chattingText(mensaje, font, 14);
+//	chattingText.setFillColor(sf::Color(0, 160, 0));
+//	chattingText.setStyle(sf::Text::Bold);
+//
+//
+//	sf::Text text(mensaje, font, 14);
+//	text.setFillColor(sf::Color(0, 160, 0));
+//	text.setStyle(sf::Text::Bold);
+//	text.setPosition(0, 560);
+//
+//	sf::RectangleShape separator(sf::Vector2f(800, 5));
+//	separator.setFillColor(sf::Color(200, 200, 200, 255));
+//	separator.setPosition(0, 550);
+//
+//	while (window.isOpen())
+//	{
+//		sf::Event evento;
+//		while (window.pollEvent(evento))
+//		{
+//			switch (evento.type)
+//			{
+//			case sf::Event::Closed:
+//				window.close();
+//				break;
+//			case sf::Event::KeyPressed:
+//				if (evento.key.code == sf::Keyboard::Escape)
+//					window.close();
+//				else if (evento.key.code == sf::Keyboard::Return)
+//				{
+//					aMensajes.push_back(mensaje);
+//					if (aMensajes.size() > 25)
+//					{
+//						aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+//					}
+//					mensaje = ">";
+//				}
+//				break;
+//			case sf::Event::TextEntered:
+//				if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+//					mensaje += (char)evento.text.unicode;
+//				else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+//					mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+//				break;
+//			}
+//		}
+//		window.draw(separator);
+//		for (size_t i = 0; i < aMensajes.size(); i++)
+//		{
+//			std::string chatting = aMensajes[i];
+//			chattingText.setPosition(sf::Vector2f(0, 20 * i));
+//			chattingText.setString(chatting);
+//			window.draw(chattingText);
+//		}
+//		std::string mensaje_ = mensaje + "_";
+//		text.setString(mensaje_);
+//		window.draw(text);
+//
+//
+//		window.display();
+//		window.clear();
+//	}
+//}
 
 int main()
 {
 	sf::Packet pack;
 
-	// WE SEND A MESSAGE TO THE SERVER SO IT KNOWS WHO WE ARE
-	udpSocket.send(pack, serverIP, serverPort);
-	while (true)
+	std::cout << "Enter server IP: ";
+	std::cin >> serverIP;
+	std::cout << "Enter server port: ";
+	std::cin >> serverPort;
+
+	socketStatus = udpSocket.send(pack, serverIP, serverPort);
+
+	std::thread tSend(&Send, serverIP, serverPort);
+	tSend.detach();
+
+	if (socketStatus != sf::UdpSocket::Status::Done)
 	{
-		udpSocket.receive(pack, serverIP, serverPort);
-
-		// TODO: HANDLE THE TYPE OF MESSAGE SENT BY THE SERVER
+		std::cout << "Connection failed." << std::endl;
 	}
+	else
+	{
+		std::cout << "Connection succesful." << std::endl;
+
+		while (true)
+		{
+			std::string message;
+
+			socketStatus = udpSocket.receive(pack, serverIP, serverPort);
 
 
+
+			if (pack >> message)
+			{
+				std::cout << message << std::endl;
+			}
+
+			// TODO: HANDLE THE TYPE OF MESSAGE SENT BY THE SERVER
+		}
+	}	
+
+	system("pause");
 	return 0;
 }
--- EjemplosClase/AA3_Server/Server.cpp
+++ EjemplosClase/AA3_Server/Server.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>  
 #include <SFML/Network.hpp>
 
 struct Client
@@ -9,31 +10,43 @@ struct Client
 };
 
 sf::UdpSocket udpSocket;
+sf::UdpSocket::Status socketStatus = sf::UdpSocket::Status::NotReady;
 std::map<std::string, Client> clientList;
 
 int main()
 {
 	std::map<std::string, Client>::iterator it = clientList.begin();
-	udpSocket.bind(50000);
+	socketStatus = udpSocket.bind(50000);
 	while (true)
 	{
 		sf::Packet pack;
 		sf::IpAddress ip;
 		unsigned short port;
 
-		udpSocket.receive(pack, ip, port);
-		std::string auxPlayerID = ip.toString() + ":" + std::to_string(port);
-		if (clientList.find(auxPlayerID) == clientList.end() )
+		socketStatus = udpSocket.receive(pack, ip, port);
+		if (socketStatus != sf::UdpSocket::Status::Done)
 		{
-			// INSERT INTO MAP
-			clientList.insert(std::pair<std::string, Client>(auxPlayerID, Client(ip,port)));			
+			// TODO: WHAT DO WE HANDLE HERE?
+			std::cout << "Socket status is " << socketStatus;
+		}
+		else
+		{
+			std::string auxPlayerID = ip.toString() + ":" + std::to_string(port);
+			if (clientList.find(auxPlayerID) == clientList.end())
+			{
+				// INSERT INTO MAP
+				clientList.insert(std::pair<std::string, Client>(auxPlayerID, Client(ip, port)));
+			}
+
+			std::cout << "Client with IP: " << ip.toString() << " and port " << port << " has sent a message: " << pack << std::endl;
+
+			// WE SEND THE PACK TO ALL CONNECTED CLIENTS
+			// FOR NOW, THIS ALSO SENDS THE PACKET TO THE ORIGINAL CLIENT SENDER
+			for (std::pair<std::string, Client> element : clientList) {
+				socketStatus = udpSocket.send(pack, element.second.ip, element.second.port);
+			}
 		}
 
-		// WE SEND THE PACK TO ALL CONNECTED CLIENTS
-		// FOR NOW, THIS ALSO SENDS THE PACKET TO THE ORIGINAL CLIENT SENDER
-		for (std::pair<std::string, Client> element : clientList) {
-			udpSocket.send(pack, element.second.ip, element.second.port);
-		}
 
 	}
 
